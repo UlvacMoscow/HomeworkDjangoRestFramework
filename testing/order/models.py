71a91dee80cc2a8d551e65e0085bc664999cf2ef
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-01-20 23:55:37 +0300

    migrate

--- testing/order/models.py
+++ testing/order/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 from customer.models import Customer
-from product.models import product
+from product.models import Product
 
 
 
 class Order(models.Model):
-    product = models.ForeignKey(product, verbose_name="Товар", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Товар", on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, verbose_name="Заказчик", on_delete=models.CASCADE)
     creater = models.DateTimeField(verbose_name="Дата создание:", auto_now_add=True)
+
+    def __str__(self):
+        return self.product.title
--- testing/product/models.py
+++ testing/product/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
 class Category(models.Model):
     #Ring, Earrings, NEcklase
     category = models.CharField(verbose_name="тип украшения", max_length=150)
 
     def __str__(self):
-        return self.type
+        return self.category
 
 class Metal(models.Model):
     metal = models.CharField(verbose_name="металл", max_length=200)
 
     def __str__(self):
-        return self.type_metal
+        return self.metal
 
 class Stone(models.Model):
     stone = models.CharField(verbose_name="камень", max_length=200)
+
+    def __str__(self):
+        return self.stone
 
 class Product(models.Model):
     title = models.CharField(verbose_name="название украшения", max_length=255)
