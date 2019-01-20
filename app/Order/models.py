a24355e41890fabfba304af9a27041f261cd04b1
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-01-20 15:14:36 +0300

    add fields Models

--- app/Order/models.py
+++ app/Order/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
-class Order():
-    pass
+class Order(models.Model):
+    decoration = models.ForeignKey(Item)
+    customer = models.ForeignKey(User)
+    creater = models.DataField(verbose_name="Дата создание:", auto_now_add=True)
--- app/shop/models.py
+++ app/shop/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
-class Category():
-    pass
+class Category(models.Model):
+    #Ring, Earrings, NEcklase
+    type = models.CharField(verbose_name="тип украшения", max_length=150)
 
+class Metal(models.Model):
+    type_metal = models.CharField(verbosa_name="металл", max_length=200)
+
+class Stone(models.Model):
+    type_stone = models.CharField(verbose_name="камень", max_length=200)
+
+class Item(models.Model):
+    title = models.Charfield(verbose_name="название украшения", max_length=255)
+    item_decoration = models.ForeignKey(Category)
+    item_metal = models.ForeignKey(Metal)
+    item_stone = models.ForeignKey(Stone)
+
+
+
+class Decoration():
+    pass
 
 class Ring():
     pass
